2c1befe731e1a03b7b2aecfe6c497d7f4e92c903
vishakhatiti <vishakhasm1234@gmail.com>
2026-07-28 00:11:18 +0530

    Initial commit

---
+++ backend/model/HoldingsModel.js
@@ -0,0 +1,6 @@
+const { model } = require("mongoose");
+const { HoldingsSchema } = require("../schemas/HoldingsSchema");
+
+const HoldingsModel = model("holding", HoldingsSchema);
+
+module.exports = { HoldingsModel };
---
+++ backend/model/PositionsModel.js
@@ -0,0 +1,6 @@
+const { model } = require("mongoose");
+const { PositionsSchema } = require("../schemas/PositionsSchema");
+
+const PositionsModel = model("position", PositionsSchema);
+
+module.exports = { PositionsModel };
